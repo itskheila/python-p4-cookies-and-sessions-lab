0a4585ef720103c0a20ab64789f8b79d273f837f
itskheila <munenetiara@gmail.com>
2025-09-27 13:45:48 +0300

    cookie sessions

--- server/app.py
+++ server/app.py
@@ -27,8 +27,13 @@ def index_articles():
 
 @app.route('/articles/<int:id>')
 def show_article(id):
-
-    pass
+    session['page_views'] = session.get('page_views', 0) + 1
+    
+    if session['page_views'] <= 3:
+        article = Article.query.filter(Article.id == id).first()
+        return article.to_dict()
+    
+    return {'message': 'Maximum pageview limit reached'}, 401
 
 if __name__ == '__main__':
     app.run(port=5555)
